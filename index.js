6d56013cad4c044a69fe2b18f7ff0e40b25ca7ad
yavrum13 <yuval.avrum@gmail.com>
2024-01-29 22:47:57 -0600

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(wrapping){
+    function result(emphatic){
+        let wrapping = '';
+        switch(emphatic){
+            case 'a hard worker':
+                wrapping = '*';
+                emphatic = 'a hard worker';
+                break;
+            case 'a dedicated programmer':
+                wrapping = '||';
+                emphatic = 'a dedicated programmer';
+                break;
+            default:
+                wrapping = '';
+                emphatic = 'special';
+                break;
+        }
+        return `You are ${wrapping}${emphatic}${wrapping}!`;
+    }
+    return result;
+
+}
